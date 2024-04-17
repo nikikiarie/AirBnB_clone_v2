ae9220f3810c3d8071f8c48f3ceaa9fd57335780
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-18 00:04:29 +0300

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Amenity Class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
