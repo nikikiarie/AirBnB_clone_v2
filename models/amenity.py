6c8191e387d39c3c113c19afe77b2c25a2213c2b
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-18 00:08:22 +0300

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+import sqlalchemy
+import models
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
