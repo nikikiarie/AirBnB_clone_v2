098a0cb268663e237ae068bdd4aaa02ee91fa7f3
nikikiarie <nix.kiariebiz@gmail.com>
2024-05-06 12:55:28 -0700

    web static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # script that sets up your web servers for the deployment of web_static
 
 sudo apt-get update
-sudo apt-get -y install ngnix
-sudo ufw allow 'Ngnix HTTP'
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
@@ -17,5 +17,5 @@ sudo echo "<html>
   </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/ngnix/sites-enabled/default
-sudo service ngnix restart
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
