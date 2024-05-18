95aa6ef2e35a99097ad7e204ab02f03ca5ce99ad
nikikiarie <nix.kiariebiz@gmail.com>
2024-05-18 14:49:57 +0300

    odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB string"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    """Return text that is formated"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Python + text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numbr(n):
+    """return n is a number if n is a valid int"""
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbr_temp(n):
+    """return temp for request"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def numbr_odd_or_even(n):
+    """display template on condition"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
